df8f3676a0edbf5901508ce41880f97e6434b200
lexiana44 <164295055+lexiana44@users.noreply.github.com>
2026-09-22 16:13:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Solucion.negocio;
+
+namespace Solucion.Presentacion
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Ingrese el número de Legajo: ");
+
+            if (int.TryParse(Console.ReadLine(), out int legajo))
+            {
+                AlumnoNegocio negocio = new AlumnoNegocio();
+                Alumno alumno = negocio.ObtenerAlumno(legajo);
+
+                if (alumno != null)
+                {
+                    Console.WriteLine($"Alumno: {alumno.Nombre}");
+                    Console.WriteLine($"Condición: {alumno.Condicion}");
+                }
+                else
+                {
+                    Console.WriteLine("Error: El legajo debe ser mayor a cero o el alumno no existe.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Debe ingresar un número entero válido.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
