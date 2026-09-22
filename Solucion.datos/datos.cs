924050979005f4b7078367793dfeeb0de44f11f6
Mary <Mary@DESKTOP-IF6BPDL>
2026-09-22 16:12:40 -0300

    Agregar archivos de proyecto.

---
+++ Solucion.datos/datos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Solucion.datos
+{
+    public class AlumnoDatos
+    {
+        private string _conexionString = "Server=localhost;Database=instituto;Uid=root;Pwd=;";
+
+        public (int Legajo, string Nombre, string Condicion)? BuscarPorLegajo(int legajo)
+        {
+            string query = "SELECT Legajo, Nombre, Condicion FROM alumno WHERE Legajo = @Legajo";
+
+            using (MySqlConnection conexion = new MySqlConnection(_conexionString))
+            {
+                MySqlCommand comando = new MySqlCommand(query, conexion);
+                comando.Parameters.AddWithValue("@Legajo", legajo);
+
+                conexion.Open();
+
+                using (MySqlDataReader reader = comando.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        int legajoDb = Convert.ToInt32(reader["Legajo"]);
+                        string nombreDb = reader["Nombre"].ToString();
+                        string condicionDb = reader["Condicion"].ToString();
+
+                        return (legajoDb, nombreDb, condicionDb);
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Solucion.negocio/negocio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Solucion.datos;
+
+namespace Solucion.negocio
+{
+    public class Alumno
+    {
+        public int Legajo { get; set; }
+        public string Nombre { get; set; }
+        public string Condicion { get; set; }
+    }
+
+    public class AlumnoNegocio
+    {
+        private AlumnoDatos _datos = new AlumnoDatos();
+
+        public Alumno ObtenerAlumno(int legajo)
+        {
+            if (legajo <= 0) return null;
+
+            var resultado = _datos.BuscarPorLegajo(legajo);
+            if (resultado == null) return null;
+
+            return new Alumno
+            {
+                Legajo = resultado.Value.Legajo,
+                Nombre = resultado.Value.Nombre,
+                Condicion = resultado.Value.Condicion
+            };
+        }
+    }
+}
